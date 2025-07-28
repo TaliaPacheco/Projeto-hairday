d3c32e9b6a2d34a8db11d6d44ada479e11b0f246
Talia.dev <taliapacheco2002@gmail.com>
2025-07-28 20:17:35 -0300

    feat: implementa serviços de API e integrações

---
+++ src/services/schedule-cancel.js
@@ -0,0 +1,13 @@
+import { apiConfig } from "./api-config.js";
+
+export async function scheduleCancel({ id }) {
+    try {
+        await fetch(`${apiConfig.baseURL}/schedules/${id}`, {
+            method: "DELETE",
+        })
+
+        alert("agendamento cancelado com sucesso")
+    } catch (error) {
+        alert ("Não foi possivel cancelar o agendameto")
+    }
+}
