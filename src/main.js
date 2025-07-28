c3ef4ca33e8866dd505a6927cfe6794b6808754e
Talia.dev <taliapacheco2002@gmail.com>
2025-07-28 20:15:41 -0300

    feat: adiciona assets e estrutura base do projeto

---
+++ src/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+//Configuração do dayjs
+import "./libs/daysjs"
+
+//css
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+//js
+import "./modules/form/submit.js"
+import "./modules/form/date-change.js"
+
+import"./modules/schedule/cancel.js"
+
+import "./modules/page-load.js"
